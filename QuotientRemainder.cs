6203adbad4fc221512f45e2d2244141afc5644b5
Deekshita1996 <113499307+Deekshita1996@users.noreply.github.com>
2022-10-18 14:55:52 +0530

    To find Quotient and Remainder
    To find Quotient and Remainder

---
+++ QuotientRemainder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class QuotientRemainder
+    {
+        public static void GetQuotientRemainder()
+        {
+            int dividend, divisor;
+            Console.WriteLine("Please enter the divident: ");
+            dividend = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the divisor");
+            divisor = int.Parse(Console.ReadLine());
+            int quotient = dividend/ divisor;
+            int remainder = dividend % divisor;
+
+            Console.WriteLine("Dividend:{0} Divisor:{1}");
+            Console.WriteLine("Quotient =" + quotient);
+            Console.WriteLine("remainder = " + remainder);
+            Console.ReadLine();
+        }
+    }
+}
