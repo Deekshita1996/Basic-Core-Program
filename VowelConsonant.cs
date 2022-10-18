c9f364c5c0d85e58f3ee3fe32d5ced6566a27746
Deekshita1996 <113499307+Deekshita1996@users.noreply.github.com>
2022-10-18 15:03:24 +0530

    To find which alphabet is Vowel and Consonant
    To find which alphabet is Vowel and Consonant

---
+++ VowelConsonant.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class VowelConsonant
+    {
+        public static void SpecifyAlphabet()
+        {
+            Console.WriteLine("Please enter the Alphabet");
+            char v = Convert.ToChar(Console.ReadLine());
+            if (v == 'a' || v == 'e' || v == 'i' || v == 'o' || v == 'u' || v == 'A' || v == 'E' || v == 'I' || v == 'O' || v == 'U')
+            {
+                Console.WriteLine("Vowel");
+            }
+            else
+            {
+                Console.WriteLine("Consonant");
+            }
+        }
+    }
+}
